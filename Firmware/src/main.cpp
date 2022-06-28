f164a6289a170f63aa6b8ed786933bb8d817b228
KenwoodFox <kenwood364@gmail.com>
2022-06-28 10:55:26 -0400

    Add firmware filler

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -7,3 +7,18 @@
  * @copyright Copyright (c) 2022
  *
  */
+
+#include "Arduino.h"
+
+void setup()
+{
+    // Setup serial
+    delay(200);
+    Serial.begin(115200);
+    Serial.println(MOTD);
+}
+
+void loop()
+{
+    ;
+}
