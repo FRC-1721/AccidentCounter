05394251b2399fab46a0e773c35648155aacd611
KenwoodFox <kenwood364@gmail.com>
2022-07-20 18:34:45 -0400

    Works enough to do something basic

--- Firmware/lib/display/display.cpp
+++ Firmware/lib/display/display.cpp
@@ -12,11 +12,14 @@ Display::Display(uint8_t _NUM_SEGMENTS, uint8_t _LATCH_PIN, uint8_t _CLOCK_PIN, 
     pinMode(latchPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
     pinMode(clockPin, OUTPUT);
+
+    setNum(10);
+    setNum(10);
 };
 
 void Display::setNum(uint16_t num)
 {
     digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, segChars[num]);
+    shiftOut(dataPin, clockPin, MSBFIRST, segChars[num]);
     digitalWrite(latchPin, HIGH);
 };
